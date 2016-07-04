a766b41c258abbfecbfddf0ca951e11e9faf6ea3
Brad Qiu <brad.qiu@finisar.com>
2016-07-04 15:11:40 +0800

    A initial submit for Promethus project

---
+++ Prometheus/Prometheus/Controllers/DashBoardController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Prometheus.Models;
+
+namespace Prometheus.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        // GET: DashBoard
+        public ActionResult ViewAll()
+        {
+            DashBoardViewModels dv = new DashBoardViewModels();
+
+            List<HtmlRaw> activeInfo = new List<HtmlRaw>();
+            var info = new HtmlRaw();
+            info.MesgContent = "<a href=\"brad.qiu@finisar.com\">Brad</a> modified file: <a href=\"~/userfiles/docs/file\">CFP4-SR4 DESIGN</a>";
+            activeInfo.Add(info);
+
+            info = new HtmlRaw();
+            info.MesgContent = "<a href=\"alex.chen@finisar.com\">Alex</a> modified file: <a href=\"~/userfiles/docs/file\">QSFP28G DESIGN</a>";
+            activeInfo.Add(info);
+
+            dv.ActiveInfoList = activeInfo;
+            return View(dv);
+        }
+
+    }
+}
---
+++ Prometheus/Prometheus/Controllers/ProjectController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prometheus.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult ViewAll()
+        {
+            return View();
+        }
+
+        public ActionResult CreateProject()
+        {
+            return View();
+        }
+
+        public ActionResult ProjectDetail()
+        {
+            return View();
+        }
+    }
+}

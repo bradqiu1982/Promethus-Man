2d2c3dfa85773851e39145a0c80d52a217ca0d0e
Brad Qiu <brad.qiu@finisar.com>
2016-08-30 17:41:10 +0800

    Fix the chart show error and fix the RMA report match error

--- Prometheus/Prometheus/Controllers/ProjectController.cs
+++ Prometheus/Prometheus/Controllers/ProjectController.cs
@@ -794,19 +794,22 @@ namespace Prometheus.Controllers
                 }
 
                 var keys = piedatadict.Keys;
-                var namevaluepair = "";
-                foreach (var k in keys)
+                if (keys.Count > 0)
                 {
-                    namevaluepair = namevaluepair+"{ name:'"+k+"',y:"+ piedatadict[k].ToString()+ "},";
+                    var namevaluepair = "";
+                    foreach (var k in keys)
+                    {
+                        namevaluepair = namevaluepair+"{ name:'"+k+"',y:"+ piedatadict[k].ToString()+ "},";
+                    }
+
+                    namevaluepair = namevaluepair.Substring(0, namevaluepair.Length - 1);
+
+                    var tempscript = System.IO.File.ReadAllText(Server.MapPath("~/Scripts/PieChart.xml"));
+                    ViewBag.chartscript = tempscript.Replace("#ElementID#", "failurepie")
+                        .Replace("#Title#", ProjectKey + "FA Realtime Failure")
+                        .Replace("#SERIESNAME#", "Failure")
+                        .Replace("#NAMEVALUEPAIRS#", namevaluepair);
                 }
-
-                namevaluepair = namevaluepair.Substring(0, namevaluepair.Length - 1);
-
-                var tempscript = System.IO.File.ReadAllText(Server.MapPath("~/Scripts/PieChart.xml"));
-                ViewBag.chartscript = tempscript.Replace("#ElementID#", "failurepie")
-                    .Replace("#Title#", ProjectKey + "FA Realtime Failure")
-                    .Replace("#SERIESNAME#", "Failure")
-                    .Replace("#NAMEVALUEPAIRS#", namevaluepair);
 
                 return View(vm);
             }
@@ -841,59 +844,61 @@ namespace Prometheus.Controllers
                 ViewBag.pjkey = ProjectKey;
 
                 var vmlist = ProjectYieldViewModule.GetYieldByWeeks(ProjectKey);
-
-                var ChartxAxisValues = "";
-                var ChartSearies = "";
-
-                var ftimelist = new List<string>();
-                var fvalues = new List<double>();
-                var cvalues = new List<double>();
-
-                foreach (var item in vmlist)
+                if (vmlist.Count > 0)
                 {
-                    ftimelist.Add(item.EndDate.ToString("yyyy-MM-dd"));
-                    fvalues.Add(item.FirstYield * 100.0);
-                    cvalues.Add(item.LastYield * 100.0);
+                    var ChartxAxisValues = "";
+                    var ChartSearies = "";
+
+                    var ftimelist = new List<string>();
+                    var fvalues = new List<double>();
+                    var cvalues = new List<double>();
+
+                    foreach (var item in vmlist)
+                    {
+                        ftimelist.Add(item.EndDate.ToString("yyyy-MM-dd"));
+                        fvalues.Add(item.FirstYield * 100.0);
+                        cvalues.Add(item.LastYield * 100.0);
+                    }
+
+                    //xaxis
+                    foreach (var item in ftimelist)
+                    {
+                        ChartxAxisValues = ChartxAxisValues + "'" + item + "',";
+                    }
+                    ChartxAxisValues = ChartxAxisValues.Substring(0, ChartxAxisValues.Length - 1);
+
+
+                    //yaxis
+                    ChartSearies = "{name:'First Yield',data:[<fvalue>]},{name:'Retest Yield',data:[<cvalue>]}";
+
+                    var tempvalue = "";
+                    foreach (var item in fvalues)
+                    {
+                        tempvalue = tempvalue + item.ToString("0.00") + ",";
+                    }
+                    tempvalue = tempvalue.Substring(0, tempvalue.Length - 1);
+                    ChartSearies = ChartSearies.Replace("<fvalue>", tempvalue);
+
+                    tempvalue = "";
+                    foreach (var item in cvalues)
+                    {
+                        tempvalue = tempvalue + item.ToString("0.00") + ",";
+                    }
+                    tempvalue = tempvalue.Substring(0, tempvalue.Length - 1);
+                    ChartSearies = ChartSearies.Replace("<cvalue>", tempvalue);
+
+                    //rederect url
+                    var reurl = "window.location.href = '/Project/ProjectWYieldDetail?ProjectKey='+document.getElementById(\"pjkey\").value+'&EndDate='+this.category";
+
+                    var tempscript = System.IO.File.ReadAllText(Server.MapPath("~/Scripts/ColumnChart.xml"));
+                    ViewBag.chartscript = tempscript.Replace("#ElementID#", "weeklyyield")
+                        .Replace("#ChartType#", "column")
+                        .Replace("#Title#", "Weekly Yiled")
+                        .Replace("#ChartxAxisValues#", ChartxAxisValues)
+                        .Replace("#yAxisTitle#", "Yield Percent")
+                        .Replace("#ChartSearies#", ChartSearies)
+                        .Replace("#REDIRECTURL#", reurl);
                 }
-
-                //xaxis
-                foreach (var item in ftimelist)
-                {
-                    ChartxAxisValues = ChartxAxisValues + "'" + item + "',";
-                }
-                ChartxAxisValues = ChartxAxisValues.Substring(0, ChartxAxisValues.Length - 1);
-
-
-                //yaxis
-                ChartSearies = "{name:'First Yield',data:[<fvalue>]},{name:'Retest Yield',data:[<cvalue>]}";
-
-                var tempvalue = "";
-                foreach (var item in fvalues)
-                {
-                    tempvalue = tempvalue + item.ToString("0.00") + ",";
-                }
-                tempvalue = tempvalue.Substring(0, tempvalue.Length - 1);
-                ChartSearies = ChartSearies.Replace("<fvalue>", tempvalue);
-
-                tempvalue = "";
-                foreach (var item in cvalues)
-                {
-                    tempvalue = tempvalue + item.ToString("0.00") + ",";
-                }
-                tempvalue = tempvalue.Substring(0, tempvalue.Length - 1);
-                ChartSearies = ChartSearies.Replace("<cvalue>", tempvalue);
-
-                //rederect url
-                var reurl = "window.location.href = '/Project/ProjectWYieldDetail?ProjectKey='+document.getElementById(\"pjkey\").value+'&EndDate='+this.category";
-
-                var tempscript = System.IO.File.ReadAllText(Server.MapPath("~/Scripts/ColumnChart.xml"));
-                ViewBag.chartscript = tempscript.Replace("#ElementID#", "weeklyyield")
-                    .Replace("#ChartType#", "column")
-                    .Replace("#Title#", "Weekly Yiled")
-                    .Replace("#ChartxAxisValues#", ChartxAxisValues)
-                    .Replace("#yAxisTitle#", "Yield Percent")
-                    .Replace("#ChartSearies#", ChartSearies)
-                    .Replace("#REDIRECTURL#", reurl);
 
                 return View();
             }
@@ -1046,59 +1051,61 @@ namespace Prometheus.Controllers
                 ViewBag.pjkey = ProjectKey;
 
                 var vmlist = ProjectYieldViewModule.GetYieldByMonth(ProjectKey);
-
-                var ChartxAxisValues = "";
-                var ChartSearies = "";
-
-                var ftimelist = new List<string>();
-                var fvalues = new List<double>();
-                var cvalues = new List<double>();
-
-                foreach (var item in vmlist)
+                if (vmlist.Count > 0)
                 {
-                    ftimelist.Add(item.EndDate.ToString("yyyy-MM-dd"));
-                    fvalues.Add(item.FirstYield * 100.0);
-                    cvalues.Add(item.LastYield * 100.0);
+                    var ChartxAxisValues = "";
+                    var ChartSearies = "";
+
+                    var ftimelist = new List<string>();
+                    var fvalues = new List<double>();
+                    var cvalues = new List<double>();
+
+                    foreach (var item in vmlist)
+                    {
+                        ftimelist.Add(item.EndDate.ToString("yyyy-MM-dd"));
+                        fvalues.Add(item.FirstYield * 100.0);
+                        cvalues.Add(item.LastYield * 100.0);
+                    }
+
+                    //xaxis
+                    foreach (var item in ftimelist)
+                    {
+                        ChartxAxisValues = ChartxAxisValues + "'" + item + "',";
+                    }
+                    ChartxAxisValues = ChartxAxisValues.Substring(0, ChartxAxisValues.Length - 1);
+
+
+                    //yaxis
+                    ChartSearies = "{name:'First Yield',data:[<fvalue>]},{name:'Retest Yield',data:[<cvalue>]}";
+
+                    var tempvalue = "";
+                    foreach (var item in fvalues)
+                    {
+                        tempvalue = tempvalue + item.ToString("0.00") + ",";
+                    }
+                    tempvalue = tempvalue.Substring(0, tempvalue.Length - 1);
+                    ChartSearies = ChartSearies.Replace("<fvalue>", tempvalue);
+
+                    tempvalue = "";
+                    foreach (var item in cvalues)
+                    {
+                        tempvalue = tempvalue + item.ToString("0.00") + ",";
+                    }
+                    tempvalue = tempvalue.Substring(0, tempvalue.Length - 1);
+                    ChartSearies = ChartSearies.Replace("<cvalue>", tempvalue);
+
+                    //rederect url
+                    var reurl = "window.location.href = '/Project/ProjectMYieldDetail?ProjectKey='+document.getElementById(\"pjkey\").value+'&EndDate='+this.category";
+
+                    var tempscript = System.IO.File.ReadAllText(Server.MapPath("~/Scripts/ColumnChart.xml"));
+                    ViewBag.chartscript = tempscript.Replace("#ElementID#", "monthlyyield")
+                        .Replace("#ChartType#", "column")
+                        .Replace("#Title#", "Monthly Yiled")
+                        .Replace("#ChartxAxisValues#", ChartxAxisValues)
+                        .Replace("#yAxisTitle#", "Yield Percent")
+                        .Replace("#ChartSearies#", ChartSearies)
+                        .Replace("#REDIRECTURL#", reurl);
                 }
-
-                //xaxis
-                foreach (var item in ftimelist)
-                {
-                    ChartxAxisValues = ChartxAxisValues + "'" + item + "',";
-                }
-                ChartxAxisValues = ChartxAxisValues.Substring(0, ChartxAxisValues.Length - 1);
-
-
-                //yaxis
-                ChartSearies = "{name:'First Yield',data:[<fvalue>]},{name:'Retest Yield',data:[<cvalue>]}";
-
-                var tempvalue = "";
-                foreach (var item in fvalues)
-                {
-                    tempvalue = tempvalue + item.ToString("0.00") + ",";
-                }
-                tempvalue = tempvalue.Substring(0, tempvalue.Length - 1);
-                ChartSearies = ChartSearies.Replace("<fvalue>", tempvalue);
-
-                tempvalue = "";
-                foreach (var item in cvalues)
-                {
-                    tempvalue = tempvalue + item.ToString("0.00") + ",";
-                }
-                tempvalue = tempvalue.Substring(0, tempvalue.Length - 1);
-                ChartSearies = ChartSearies.Replace("<cvalue>", tempvalue);
-
-                //rederect url
-                var reurl = "window.location.href = '/Project/ProjectMYieldDetail?ProjectKey='+document.getElementById(\"pjkey\").value+'&EndDate='+this.category";
-
-                var tempscript = System.IO.File.ReadAllText(Server.MapPath("~/Scripts/ColumnChart.xml"));
-                ViewBag.chartscript = tempscript.Replace("#ElementID#", "monthlyyield")
-                    .Replace("#ChartType#", "column")
-                    .Replace("#Title#", "Monthly Yiled")
-                    .Replace("#ChartxAxisValues#", ChartxAxisValues)
-                    .Replace("#yAxisTitle#", "Yield Percent")
-                    .Replace("#ChartSearies#", ChartSearies)
-                    .Replace("#REDIRECTURL#", reurl);
 
                 return View();
             }

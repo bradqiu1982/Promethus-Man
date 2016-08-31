21e17b6a2a1d2fae091f27beae169cae8a23821e
Brad Qiu <brad.qiu@finisar.com>
2016-08-31 16:53:30 +0800

    Add Pareto chart into monthly and period yield detail page

---
+++ Prometheus/Prometheus/Models/ParetoData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prometheus.Models
+{
+    public class ParetoData
+    {
+        public string key { set; get; }
+        public int count { set; get; }
+        public double percent { set; get; }
+        public double sumpercent { set; get; }
+    }
+}

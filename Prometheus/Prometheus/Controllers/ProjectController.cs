4600bbddc36c646fbcad00bb66b1d35283d657cb
Brad Qiu <brad.qiu@finisar.com>
2016-09-01 17:40:21 +0800

    Add pareto into weekly yield detail page

--- Prometheus/Prometheus/Controllers/ProjectController.cs
+++ Prometheus/Prometheus/Controllers/ProjectController.cs
@@ -924,6 +924,9 @@ namespace Prometheus.Controllers
                     }
                 }
 
+                var firstdatalist = new List<KeyValuePair<string, int>>();
+                var retestdatalist = new List<KeyValuePair<string, int>>();
+
                 var pvm = ProjectViewModels.RetrieveOneProject(ProjectKey);
                 var yieldvm = ProjectYieldViewModule.GetYieldByDateRange(ProjectKey, sdate.ToString(), edate.ToString(),pvm);
 
@@ -956,6 +959,8 @@ namespace Prometheus.Controllers
                             }
                         }
                     }
+
+                    firstdatalist = piedatadict.ToList();
 
                     piedatadict["PASS"] = ProjectYieldViewModule.RetrieveErrorCount("PASS", yieldvm.FirstYields[yieldvm.FirstYields.Count - 1].WhichTest, yieldvm.FErrorMap);
 
@@ -1007,6 +1012,8 @@ namespace Prometheus.Controllers
                         }
                     }
 
+                    retestdatalist = piedatadict.ToList();
+
                     piedatadict["PASS"] = ProjectYieldViewModule.RetrieveErrorCount("PASS", yieldvm.LastYields[yieldvm.LastYields.Count - 1].WhichTest, yieldvm.LErrorMap);
 
                     var keys = piedatadict.Keys;
@@ -1024,6 +1031,176 @@ namespace Prometheus.Controllers
                         .Replace("#Title#", "Retest Failure")
                         .Replace("#SERIESNAME#", "RFailure")
                         .Replace("#NAMEVALUEPAIRS#", namevaluepair);
+                }
+
+                if (firstdatalist.Count > 0)
+                {
+                    var peralist = new List<ParetoData>();
+
+                    if (firstdatalist.Count > 1)
+                    {
+                        firstdatalist.Sort(delegate (KeyValuePair<string, int> pair1, KeyValuePair<string, int> pair2)
+                        {
+                            return pair2.Value.CompareTo(pair1.Value);
+                        });
+                    }
+
+                    var sum = 0;
+                    for (var i = 0; i < firstdatalist.Count; i++)
+                    {
+                        sum = sum + firstdatalist[i].Value;
+                    }
+
+                    for (var i = 0; i < firstdatalist.Count; i++)
+                    {
+                        var tempperato = new ParetoData();
+                        tempperato.key = firstdatalist[i].Key;
+                        if (i == 0)
+                        {
+                            tempperato.count = firstdatalist[i].Value;
+                            tempperato.percent = tempperato.count / (double)sum;
+                            tempperato.sumpercent = tempperato.percent;
+                            peralist.Add(tempperato);
+                        }
+                        else
+                        {
+                            tempperato.count = firstdatalist[i].Value;
+                            tempperato.percent = tempperato.count / (double)sum;
+                            tempperato.sumpercent = peralist[peralist.Count - 1].sumpercent + tempperato.percent;
+                            peralist.Add(tempperato);
+                        }
+                    }
+
+                    //xaxis
+                    var ChartxAxisValues = "";
+
+                    foreach (var item in peralist)
+                    {
+                        ChartxAxisValues = ChartxAxisValues + "'" + item.key + "',";
+                    }
+                    ChartxAxisValues = ChartxAxisValues.Substring(0, ChartxAxisValues.Length - 1);
+
+
+                    //yaxis
+                    //var ChartSearies = "{name:'Defect',data:[<fvalue>]}";
+
+                    var pcountvalue = "";
+                    foreach (var item in peralist)
+                    {
+                        pcountvalue = pcountvalue + item.count.ToString() + ",";
+                    }
+                    pcountvalue = pcountvalue.Substring(0, pcountvalue.Length - 1);
+
+                    var ppecentvalue = "";
+                    foreach (var item in peralist)
+                    {
+                        ppecentvalue = ppecentvalue + (item.sumpercent * 100).ToString("0.0") + ",";
+                    }
+                    ppecentvalue = ppecentvalue.Substring(0, ppecentvalue.Length - 1);
+
+                    var abpecentvalue = "";
+                    foreach (var item in peralist)
+                    {
+                        abpecentvalue = abpecentvalue + (item.percent * 100).ToString("0.0") + ",";
+                    }
+                    abpecentvalue = abpecentvalue.Substring(0, abpecentvalue.Length - 1);
+
+                    //ChartSearies = ChartSearies.Replace("<fvalue>", tempvalue);
+
+                    var tempscript = System.IO.File.ReadAllText(Server.MapPath("~/Scripts/ParetoChart.xml"));
+                    ViewBag.fparetoscript = tempscript.Replace("#ElementID#", "fparetochart")
+                        .Replace("#Title#", "Pareto of First Test Defect")
+                        .Replace("#XAxisTitle#", "Defect")
+                        .Replace("#ChartxAxisValues#", ChartxAxisValues)
+                        .Replace("#AmountMAX#", sum.ToString())
+                        .Replace("#PCount#", pcountvalue)
+                        .Replace("#ABPercent#", abpecentvalue)
+                        .Replace("#PPercent#", ppecentvalue);
+                }
+
+                if (retestdatalist.Count > 0)
+                {
+                    var peralist = new List<ParetoData>();
+
+                    if (retestdatalist.Count > 1)
+                    {
+                        retestdatalist.Sort(delegate (KeyValuePair<string, int> pair1, KeyValuePair<string, int> pair2)
+                        {
+                            return pair2.Value.CompareTo(pair1.Value);
+                        });
+                    }
+
+                    var sum = 0;
+                    for (var i = 0; i < retestdatalist.Count; i++)
+                    {
+                        sum = sum + retestdatalist[i].Value;
+                    }
+
+                    for (var i = 0; i < retestdatalist.Count; i++)
+                    {
+                        var tempperato = new ParetoData();
+                        tempperato.key = retestdatalist[i].Key;
+                        if (i == 0)
+                        {
+                            tempperato.count = retestdatalist[i].Value;
+                            tempperato.percent = tempperato.count / (double)sum;
+                            tempperato.sumpercent = tempperato.percent;
+                            peralist.Add(tempperato);
+                        }
+                        else
+                        {
+                            tempperato.count = retestdatalist[i].Value;
+                            tempperato.percent = tempperato.count / (double)sum;
+                            tempperato.sumpercent = peralist[peralist.Count - 1].sumpercent + tempperato.percent;
+                            peralist.Add(tempperato);
+                        }
+                    }
+
+                    //xaxis
+                    var ChartxAxisValues = "";
+
+                    foreach (var item in peralist)
+                    {
+                        ChartxAxisValues = ChartxAxisValues + "'" + item.key + "',";
+                    }
+                    ChartxAxisValues = ChartxAxisValues.Substring(0, ChartxAxisValues.Length - 1);
+
+
+                    //yaxis
+                    //var ChartSearies = "{name:'Defect',data:[<fvalue>]}";
+
+                    var pcountvalue = "";
+                    foreach (var item in peralist)
+                    {
+                        pcountvalue = pcountvalue + item.count.ToString() + ",";
+                    }
+                    pcountvalue = pcountvalue.Substring(0, pcountvalue.Length - 1);
+
+                    var ppecentvalue = "";
+                    foreach (var item in peralist)
+                    {
+                        ppecentvalue = ppecentvalue + (item.sumpercent * 100).ToString("0.0") + ",";
+                    }
+                    ppecentvalue = ppecentvalue.Substring(0, ppecentvalue.Length - 1);
+
+                    var abpecentvalue = "";
+                    foreach (var item in peralist)
+                    {
+                        abpecentvalue = abpecentvalue + (item.percent * 100).ToString("0.0") + ",";
+                    }
+                    abpecentvalue = abpecentvalue.Substring(0, abpecentvalue.Length - 1);
+
+                    //ChartSearies = ChartSearies.Replace("<fvalue>", tempvalue);
+
+                    var tempscript = System.IO.File.ReadAllText(Server.MapPath("~/Scripts/ParetoChart.xml"));
+                    ViewBag.rparetoscript = tempscript.Replace("#ElementID#", "rparetochart")
+                        .Replace("#Title#", "Pareto of Retest Defect")
+                        .Replace("#XAxisTitle#", "Defect")
+                        .Replace("#ChartxAxisValues#", ChartxAxisValues)
+                        .Replace("#AmountMAX#", sum.ToString())
+                        .Replace("#PCount#", pcountvalue)
+                        .Replace("#ABPercent#", abpecentvalue)
+                        .Replace("#PPercent#", ppecentvalue);
                 }
 
                 return View(yieldvm);

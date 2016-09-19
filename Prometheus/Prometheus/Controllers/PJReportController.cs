217ced05bdbbfd8af680f06e7179ca5c2d25c73c
Brad Qiu <brad.qiu@finisar.com>
2016-09-19 18:01:15 +0800

    Add PJReport Controller which can enable user configure and review the report which they have interesting

---
+++ Prometheus/Prometheus/Controllers/PJReportController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Prometheus.Models;
+
+namespace Prometheus.Controllers
+{
+    public class PJReportController : Controller
+    {
+
+        private List<SelectListItem> CreateSelectList(List<string> valist, string defVal)
+        {
+            var pslist = new List<SelectListItem>();
+            foreach (var p in valist)
+            {
+                var pitem = new SelectListItem();
+                pitem.Text = p;
+                pitem.Value = p;
+                if (!string.IsNullOrEmpty(defVal) && string.Compare(defVal, p) == 0)
+                {
+                    pitem.Selected = true;
+                }
+                pslist.Add(pitem);
+            }
+
+            if (string.IsNullOrEmpty(defVal) && pslist.Count > 0)
+            {
+                pslist[0].Selected = true;
+            }
+
+            return pslist;
+        }
+
+        // GET: PJReport
+        public ActionResult IBook()
+        {
+            var ckdict = CookieUtility.UnpackCookie(this);
+            if (ckdict.ContainsKey("logonuser") && !string.IsNullOrEmpty(ckdict["logonuser"]))
+            {
+
+            }
+            else
+            {
+                var ck = new Dictionary<string, string>();
+                ck.Add("logonredirectctrl", "PJReport");
+                ck.Add("logonredirectact", "IBook");
+                CookieUtility.SetCookie(this, ck);
+                return RedirectToAction("LoginUser", "User");
+            }
+
+            ViewBag.PJList = ProjectViewModels.RetrieveAllProjectKey();
+
+            var temptypelist = new List<string>();
+            temptypelist.Add(PJReportType.YieldTrend);
+            temptypelist.Add(PJReportType.IssueTrend);
+            temptypelist.Add(PJReportType.FailureTrend);
+            temptypelist.Add(PJReportType.MonthlyPareto);
+            ViewBag.RepTypeList = temptypelist;
+
+            var username = ckdict["logonuser"].Split(new char[] { '|' })[0];
+            var vm = PJReportViewModels.RetrieveBookReportRecord(username);
+
+            var rsilist = new List<string>();
+            string[] rlist = { ReportSelectTime.MON, ReportSelectTime.TUE, ReportSelectTime.WEN, ReportSelectTime.THU, ReportSelectTime.FRI, ReportSelectTime.SAT, ReportSelectTime.SUN, ReportSelectTime.EVE };
+            rsilist.AddRange(rlist);
+            if (vm != null)
+            {
+                ViewBag.ReportTimeList = CreateSelectList(rsilist, vm.reporttime);
+            }
+            else
+            {
+                ViewBag.ReportTimeList = CreateSelectList(rsilist,"");
+            }
+            
+            return View(vm);
+        }
+
+        [HttpPost, ActionName("IBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult IBookPost()
+        {
+            var ckdict = CookieUtility.UnpackCookie(this);
+            var updater = ckdict["logonuser"].Split(new char[] { '|' })[0];
+
+            var vm = new PJReportViewModels();
+            vm.username = updater;
+
+            for (var i = 0; i < 20; i++)
+            {
+                if (Request.Form["pckbox" + i] != null && string.Compare(Request.Form["pckbox" + i], "true", true) == 0)
+                {
+                    vm.ProjectList.Add(Request.Form["PJKEY" + i]);
+                }
+            }
+
+            for (var i = 0; i < 20; i++)
+            {
+                if (Request.Form["rckbox" + i] != null && string.Compare(Request.Form["rckbox" + i], "true", true) == 0)
+                {
+                    vm.ReportTypeList.Add(Request.Form["RPType" + i]);
+                }
+            }
+
+            vm.reporttime = Request.Form["ReportTimeList"].ToString();
+            vm.lastupdatetime = DateTime.Now;
+            vm.StoreBookReportRecord();
+
+
+            ViewBag.PJList = ProjectViewModels.RetrieveAllProjectKey();
+
+            var temptypelist = new List<string>();
+            temptypelist.Add(PJReportType.YieldTrend);
+            temptypelist.Add(PJReportType.IssueTrend);
+            temptypelist.Add(PJReportType.FailureTrend);
+            temptypelist.Add(PJReportType.MonthlyPareto);
+            ViewBag.RepTypeList = temptypelist;
+
+            vm = PJReportViewModels.RetrieveBookReportRecord(updater);
+            var rsilist = new List<string>();
+            string[] rlist = { ReportSelectTime.MON, ReportSelectTime.TUE, ReportSelectTime.WEN, ReportSelectTime.THU, ReportSelectTime.FRI, ReportSelectTime.SAT, ReportSelectTime.SUN, ReportSelectTime.EVE };
+            rsilist.AddRange(rlist);
+            if (vm != null)
+            {
+                ViewBag.ReportTimeList = CreateSelectList(rsilist, vm.reporttime);
+            }
+            else
+            {
+                ViewBag.ReportTimeList = CreateSelectList(rsilist, "");
+            }
+
+            return View(vm);
+        }
+
+        public ActionResult ViewReport()
+        {
+
+        }
+    }
+}
---
+++ Prometheus/Prometheus/Models/PJReportViewModels.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prometheus.Models
+{
+    public class ReportSelectTime
+    {
+        public static string MON = "MONDAY";
+        public static string TUE = "TUESDAY";
+        public static string WEN = "WENDSDAY";
+        public static string THU = "THURSDAY";
+        public static string FRI = "FRIDAY";
+        public static string SAT = "SATURDAY";
+        public static string SUN = "SUNDAY";
+        public static string EVE = "EVERY DAY";
+
+        public int GetWeekDay(string day)
+        {
+            if (string.Compare(day, ReportSelectTime.MON) == 0)
+            {
+                return (int)DayOfWeek.Monday;
+            }
+            if (string.Compare(day, ReportSelectTime.TUE) == 0)
+            {
+                return (int)DayOfWeek.Tuesday;
+            }
+            if (string.Compare(day, ReportSelectTime.WEN) == 0)
+            {
+                return (int)DayOfWeek.Wednesday;
+            }
+            if (string.Compare(day, ReportSelectTime.THU) == 0)
+            {
+                return (int)DayOfWeek.Thursday;
+            }
+            if (string.Compare(day, ReportSelectTime.FRI) == 0)
+            {
+                return (int)DayOfWeek.Friday;
+            }
+            if (string.Compare(day, ReportSelectTime.SAT) == 0)
+            {
+                return (int)DayOfWeek.Saturday;
+            }
+            if (string.Compare(day, ReportSelectTime.SUN) == 0)
+            {
+                return (int)DayOfWeek.Sunday;
+            }
+            return 8;
+        }
+    }
+
+    public class PJReportType
+    {
+        public static string YieldTrend = "Yield Trend";
+        public static string IssueTrend = "Issue Solved Trend";
+        public static string FailureTrend = "Failure Trend";
+        public static string MonthlyPareto = "Monthly Pareto";
+    }
+
+
+    public class PJReportViewModels
+    {
+        public string username { set; get; }
+
+        private List<string> projectlist = new List<string>();
+
+        public List<string> ProjectList
+        {
+            get
+            { return projectlist; }
+            set
+            {
+                projectlist.Clear();
+                projectlist.AddRange(value);
+            }
+        }
+
+        public string projects
+        {
+            set
+            {
+                projectlist.Clear();
+                if (!string.IsNullOrEmpty(value))
+                {
+                    var temppn = value.Replace("'", "");
+                    var temppns = temppn.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var p in temppns)
+                    {
+                        projectlist.Add(p.Trim());
+                    }
+                }
+            }
+
+            get
+            {
+                var ret = "";
+                foreach (var p in projectlist)
+                {
+                    if (string.IsNullOrEmpty(ret))
+                        ret = p;
+                    else
+                        ret = ret + ";" + p;
+                }
+                return ret;
+            }
+        }
+
+
+        private List<string> reporttypelist = new List<string>();
+
+        public List<string> ReportTypeList
+        {
+            get
+            { return reporttypelist; }
+            set
+            {
+                reporttypelist.Clear();
+                reporttypelist.AddRange(value);
+            }
+        }
+
+        public string reporttypes
+        {
+            set
+            {
+                reporttypelist.Clear();
+                if (!string.IsNullOrEmpty(value))
+                {
+                    var temppn = value.Replace("'", "");
+                    var temppns = temppn.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var p in temppns)
+                    {
+                        reporttypelist.Add( p.Trim());
+                    }
+                }
+            }
+
+            get
+            {
+                var ret = "";
+                foreach (var p in reporttypelist)
+                {
+                    if (string.IsNullOrEmpty(ret))
+                        ret = p;
+                    else
+                        ret = ret + ";" + p;
+                }
+                return ret;
+            }
+        }
+
+
+        public string reporttime { set; get; }
+
+        public DateTime lastupdatetime { set; get; }
+
+        public void StoreBookReportRecord()
+        {
+            if (!string.IsNullOrEmpty(projects) && !string.IsNullOrEmpty(reporttypes))
+            {
+                var sql = "delete from BookReportRecord where username = '<username>'";
+                sql = sql.Replace("<username>", username);
+                DBUtility.ExeLocalSqlNoRes(sql);
+
+                sql = "insert into BookReportRecord(username,projects,reporttypes,reporttime,lastupdatetime) values('<username>','<projects>','<reporttypes>','<reporttime>','<lastupdatetime>')";
+                sql = sql.Replace("<username>", username).Replace("<projects>", projects)
+                    .Replace("<reporttypes>", reporttypes).Replace("<reporttime>", reporttime).Replace("<lastupdatetime>", lastupdatetime.ToString());
+                DBUtility.ExeLocalSqlNoRes(sql);
+            }
+        }
+
+        public static PJReportViewModels RetrieveBookReportRecord(string name)
+        {
+            var sql = "select username,projects,reporttypes,reporttime,lastupdatetime from BookReportRecord where username = '<username>'";
+            sql = sql.Replace("<username>", name);
+            var dbret = DBUtility.ExeLocalSqlWithRes(sql);
+            if (dbret.Count > 0)
+            {
+                var ret = new PJReportViewModels();
+                ret.username = Convert.ToString(dbret[0][0]);
+                ret.projects = Convert.ToString(dbret[0][1]);
+                ret.reporttypes = Convert.ToString(dbret[0][2]);
+                ret.reporttime = Convert.ToString(dbret[0][3]);
+                ret.lastupdatetime = DateTime.Parse(Convert.ToString(dbret[0][4]));
+                return ret;
+            }
+            return null;
+        }
+
+    }
+}

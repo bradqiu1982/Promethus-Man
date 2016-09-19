6cb9fe7f384767fdb2840cdc0edc4e8f63ff5636
Brad Qiu <brad.qiu@finisar.com>
2016-09-19 10:05:17 +0800

    Tried to send webpage in email but fail

---
+++ Prometheus/Prometheus/Models/WebPageVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Prometheus.Models
+{
+    public class WebPageVM
+    {
+        public static string GetPage(string urlAddress)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, System.Text.Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                string data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+                return data;
+            }
+            return string.Empty;
+        }
+    }
+}

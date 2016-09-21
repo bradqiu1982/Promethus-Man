f3eca7d79c25e741cb9140e139aaefb6324835fb
Brad Qiu <brad.qiu@finisar.com>
2016-09-21 18:23:16 +0800

    Add a ShareData controller which can share jason style data to others

---
+++ Prometheus/Prometheus/Controllers/ShareDataController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Prometheus.Controllers
+{
+    public class ShareDataController : ApiController
+    {
+        [HttpGet]
+        public Dictionary<string, List<string>> DataMap()
+        {
+            var plist = new Dictionary<string, List<string>>();
+            var slist = new List<string>();
+            slist.Add("apple");
+            slist.Add("banana");
+            plist.Add("fruit", slist);
+            slist = new List<string>();
+            slist.Add("rice");
+            slist.Add("fish");
+            plist.Add("food", slist);
+
+            return plist;
+        }
+    }
+}

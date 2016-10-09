ab5d39b2499a1b1cffce65c0f3209bbad309a873
Brad Qiu <brad.qiu@finisar.com>
2016-10-09 15:09:20 +0800

    Combine HeartBeate execute file into the site startup

--- Prometheus/Prometheus/Global.asax.cs
+++ Prometheus/Prometheus/Global.asax.cs
@@ -6,7 +6,8 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Http;
-
+using System.Diagnostics;
+using System.IO;
 
 namespace Prometheus
 {
@@ -19,6 +20,20 @@ namespace Prometheus
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            try
+            {
+                using (Process myprocess = new Process())
+                {
+                    myprocess.StartInfo.FileName = Path.Combine(System.Web.Hosting.HostingEnvironment.ApplicationPhysicalPath, @"Scripts\HeartBeat.exe").Replace("\\", "/");
+                    //System.Windows.MessageBox.Show(myprocess.StartInfo.FileName);
+                    //myprocess.StartInfo.CreateNoWindow = true;
+                    myprocess.Start();
+                }
+            }
+            catch (Exception ex)
+            { }
+
         }
     }
 }

5dd875a0c5e99c78537ea963df3b70596e910621
Brad Qiu <brad.qiu@finisar.com>
2017-04-28 15:19:41 +0800

    When debug applicate will not start headbeat application

--- Prometheus/Prometheus/Global.asax.cs
+++ Prometheus/Prometheus/Global.asax.cs
@@ -13,6 +13,17 @@ namespace Prometheus
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private bool IsDebug()
+        {
+            bool debugging = false;
+#if DEBUG
+            debugging = true;
+#else
+            debugging = false;
+#endif
+            return debugging;
+        }
+
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
@@ -23,12 +34,15 @@ namespace Prometheus
 
             try
             {
-                using (Process myprocess = new Process())
+                if (!IsDebug())
                 {
-                    myprocess.StartInfo.FileName = Path.Combine(System.Web.Hosting.HostingEnvironment.ApplicationPhysicalPath, @"Scripts\HeartBeat.exe").Replace("\\", "/");
-                    //System.Windows.MessageBox.Show(myprocess.StartInfo.FileName);
-                    //myprocess.StartInfo.CreateNoWindow = true;
-                    myprocess.Start();
+                    using (Process myprocess = new Process())
+                    {
+                        myprocess.StartInfo.FileName = Path.Combine(System.Web.Hosting.HostingEnvironment.ApplicationPhysicalPath, @"Scripts\HeartBeat.exe").Replace("\\", "/");
+                        //System.Windows.MessageBox.Show(myprocess.StartInfo.FileName);
+                        //myprocess.StartInfo.CreateNoWindow = true;
+                        myprocess.Start();
+                    }
                 }
             }
             catch (Exception ex)

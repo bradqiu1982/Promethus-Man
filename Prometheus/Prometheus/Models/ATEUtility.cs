2de0b34beaad8641a563ca8e8b81d2eaa31fbad2
Brad Qiu <brad.qiu@finisar.com>
2017-07-06 15:47:42 +0800

    Add DataField into the comment of ATE database project FA task

--- Prometheus/Prometheus/Models/ATEUtility.cs
+++ Prometheus/Prometheus/Models/ATEUtility.cs
@@ -6,6 +6,20 @@ using System.Web;
 
 namespace Prometheus.Models
 {
+    public class TempDataSetItem
+    {
+        public TempDataSetItem(string ds, string status,string tempdatasetid)
+        {
+            DatasetName = ds;
+            Status = status;
+            DatasetID = tempdatasetid;
+        }
+
+        public string DatasetName { set; get; }
+        public string Status { set; get; }
+        public string DatasetID { set; get; }
+    }
+
     public class ATEUtility
     {
         public static void retrieveATEData()
@@ -48,13 +62,13 @@ namespace Prometheus.Models
                     mdcond = mdcond.Substring(0, mdcond.Length - 4) + ")";
 
 
-                    var s  = "SELECT a.MFR_SN,d.DATASET_NAME,c.FAMILY,d.STATUS,d.END_TIME,d.STATION,a.MFR_PN,d.ROUTE_ID FROM PARTS a INNER JOIN ROUTES b ON a.OPT_INDEX = b.PART_INDEX INNER JOIN BOM_CONTEXT_ID c ON c.BOM_CONTEXT_ID = b.BOM_CONTEXT_ID INNER JOIN DATASETS d ON b.ROUTE_ID = d.ROUTE_ID WHERE <mdcond> and d.END_TIME > '<TIMECOND>'  ORDER BY d.ROUTE_ID,d.END_TIME DESC";
+                    var s  = "SELECT a.MFR_SN,d.DATASET_NAME,c.FAMILY,d.STATUS,d.END_TIME,d.STATION,a.MFR_PN,d.ROUTE_ID,d.dataset_id FROM PARTS a INNER JOIN ROUTES b ON a.OPT_INDEX = b.PART_INDEX INNER JOIN BOM_CONTEXT_ID c ON c.BOM_CONTEXT_ID = b.BOM_CONTEXT_ID INNER JOIN DATASETS d ON b.ROUTE_ID = d.ROUTE_ID WHERE <mdcond> and d.END_TIME > '<TIMECOND>'  ORDER BY d.ROUTE_ID,d.dataset_id DESC,d.END_TIME DESC";
                     var sndict = new Dictionary<string, bool>();
                     var sql = s.Replace("<TIMECOND>", vm.StartDate.ToString("yyyyMMddhhmmss")).Replace("<mdcond>", mdcond);
 
 
                     var currentroutid = "";
-                    var currentroutedsnames = new List<ProjectStation>();
+                    var currentroutedsnames = new List<TempDataSetItem>();
                     var pjdatalist = new List<ProjectTestData>();
 
                     var dbret = DBUtility.ExeATESqlWithRes(sql);
@@ -66,6 +80,7 @@ namespace Prometheus.Models
                                 var status = Convert.ToString(item[3]);
                                 var ds = Convert.ToString(item[1]);
                                 var temprouteid = Convert.ToString(item[7]);
+                            var tempdatasetid = Convert.ToString(item[8]);
 
                             if (string.Compare(currentroutid, temprouteid) != 0)
                             {
@@ -107,11 +122,11 @@ namespace Prometheus.Models
 
                                 pjdatalist.Clear();
                                 currentroutedsnames.Clear();
-                                currentroutedsnames.Add(new ProjectStation(ds, status));
+                                currentroutedsnames.Add(new TempDataSetItem(ds, status,tempdatasetid));
                             }
                             else //if (tempdata != null)
                             {
-                                currentroutedsnames.Add(new ProjectStation(ds, status));
+                                currentroutedsnames.Add(new TempDataSetItem(ds, status,tempdatasetid));
                             }
 
                             if (dsdict.ContainsKey(ds))
@@ -178,9 +193,10 @@ namespace Prometheus.Models
         }
 
 
-        public static void FindErrAbbr(List<ProjectTestData> pjdatalist, List<ProjectStation> currentroutedsnames, Dictionary<string, bool> dsdict)
+        public static void FindErrAbbr(List<ProjectTestData> pjdatalist, List<TempDataSetItem> currentroutedsnames, Dictionary<string, bool> dsdict)
         {
             var startidx = 0;
+
             foreach (var pjdata in pjdatalist)
             {
                 if (string.Compare(pjdata.ErrAbbr, "PASS", true) == 0)
@@ -188,26 +204,44 @@ namespace Prometheus.Models
                     continue;
                 }
 
+                bool found = false;
+                var maindatasetid = Convert.ToInt64(pjdata.DataID);
+
                 for (var idx = startidx; idx < currentroutedsnames.Count; idx++)
                 {
-                    if (string.Compare(currentroutedsnames[idx].Station, "PASS", true) != 0
-                        && string.Compare(currentroutedsnames[idx].Station, "INFO", true) != 0
-                        && !dsdict.ContainsKey(currentroutedsnames[idx].ProjectKey))
+                    if (string.Compare(currentroutedsnames[idx].Status, "PASS", true) != 0
+                        && string.Compare(currentroutedsnames[idx].Status, "INFO", true) != 0
+                        && !dsdict.ContainsKey(currentroutedsnames[idx].DatasetName))
                     {
-                        pjdata.ErrAbbr = currentroutedsnames[idx].ProjectKey;
-                        startidx = idx + 1;
-                        break;
+                        var subdatasetid = Convert.ToInt64(currentroutedsnames[idx].DatasetID);
+                        if (subdatasetid < maindatasetid)
+                        {
+                            pjdata.ErrAbbr = currentroutedsnames[idx].DatasetName;
+                            pjdata.DataID = currentroutedsnames[idx].DatasetID;
+                            found = true;
+                            
+                        }
                     }
 
-                    //find the stard position of next indivition dataset
-                    for (var tmpidx = startidx; tmpidx < currentroutedsnames.Count; tmpidx++)
+                    if (dsdict.ContainsKey(currentroutedsnames[idx].DatasetName))
                     {
-                        if (dsdict.ContainsKey(currentroutedsnames[tmpidx].ProjectKey))
+                        var nextdatasetid = Convert.ToInt64(currentroutedsnames[idx].DatasetID);
+                        if ((nextdatasetid < maindatasetid) && found)
                         {
-                            startidx = tmpidx;
+                            startidx = idx+1;
                             break;
                         }
-                    }//end for
+                    }
+
+                    ////find the stard position of next indivition dataset
+                    //for (var tmpidx = startidx; tmpidx < currentroutedsnames.Count; tmpidx++)
+                    //{
+                    //    if (dsdict.ContainsKey(currentroutedsnames[tmpidx].DatasetName))
+                    //    {
+                    //        startidx = tmpidx;
+                    //        break;
+                    //    }
+                    //}//end for
                 }
             }
         }
@@ -238,12 +272,12 @@ namespace Prometheus.Models
                     mdcond = mdcond.Substring(0, mdcond.Length - 4) + ")";
 
 
-                    var s = "SELECT a.MFR_SN,d.DATASET_NAME,c.FAMILY,d.STATUS,d.END_TIME,d.STATION,a.MFR_PN,d.ROUTE_ID FROM PARTS a INNER JOIN ROUTES b ON a.OPT_INDEX = b.PART_INDEX INNER JOIN BOM_CONTEXT_ID c ON c.BOM_CONTEXT_ID = b.BOM_CONTEXT_ID INNER JOIN DATASETS d ON b.ROUTE_ID = d.ROUTE_ID WHERE <mdcond> and d.END_TIME > '<TIMECOND>'  ORDER BY d.ROUTE_ID,d.END_TIME DESC";
+                    var s = "SELECT a.MFR_SN,d.DATASET_NAME,c.FAMILY,d.STATUS,d.END_TIME,d.STATION,a.MFR_PN,d.ROUTE_ID,d.dataset_id FROM PARTS a INNER JOIN ROUTES b ON a.OPT_INDEX = b.PART_INDEX INNER JOIN BOM_CONTEXT_ID c ON c.BOM_CONTEXT_ID = b.BOM_CONTEXT_ID INNER JOIN DATASETS d ON b.ROUTE_ID = d.ROUTE_ID WHERE <mdcond> and d.END_TIME > '<TIMECOND>'  ORDER BY d.ROUTE_ID,d.dataset_id DESC,d.END_TIME DESC";
                     var sndict = new Dictionary<string, bool>();
                     var sql = s.Replace("<TIMECOND>", DateTime.Parse(starttime).ToString("yyyyMMddhhmmss")).Replace("<mdcond>", mdcond);
 
                     var currentroutid = "";
-                    var currentroutedsnames = new List<ProjectStation>();
+                    var currentroutedsnames = new List<TempDataSetItem>();
                     var pjdatalist = new List<ProjectTestData>();
 
                     var dbret = DBUtility.ExeATESqlWithRes(sql);
@@ -255,6 +289,7 @@ namespace Prometheus.Models
                             var status = Convert.ToString(item[3]);
                             var ds = Convert.ToString(item[1]);
                             var temprouteid = Convert.ToString(item[7]);
+                            var tempdatasetid = Convert.ToString(item[8]);
 
                             if (string.Compare(currentroutid, temprouteid) != 0)
                             {
@@ -297,11 +332,11 @@ namespace Prometheus.Models
 
                                 pjdatalist.Clear();
                                 currentroutedsnames.Clear();
-                                currentroutedsnames.Add(new ProjectStation(ds, status));
+                                currentroutedsnames.Add(new TempDataSetItem(ds, status,tempdatasetid));
                             }
                             else //if (tempdata != null)
                             {
-                                currentroutedsnames.Add(new ProjectStation(ds, status));
+                                currentroutedsnames.Add(new TempDataSetItem(ds, status,tempdatasetid));
                             }
 
                             if (dsdict.ContainsKey(ds))
@@ -314,6 +349,7 @@ namespace Prometheus.Models
                                 var tempdata = new ProjectTestData(vm.ProjectKey, GetUniqKey(), Convert.ToString(item[0])
                                             , Convert.ToString(item[1]), Convert.ToString(item[2]), Convert.ToString(item[3]).ToUpper()
                                             , stdtime, Convert.ToString(item[5]), Convert.ToString(item[6]));
+                                tempdata.DataID = tempdatasetid;
 
                                 pjdatalist.Add(tempdata);
                             }
@@ -385,6 +421,51 @@ namespace Prometheus.Models
 
         }
 
+        private static string DataFieldStr(string datasetid)
+        {
+            var ret = string.Empty;
+            var sql = "select data_name,data_val1,data_val2,data_val3 from route_data where dataset_id = '<DataSetID>'";
+            sql = sql.Replace("<DataSetID>", datasetid);
+            var dbret = DBUtility.ExeATESqlWithRes(sql);
+            if (dbret.Count > 0)
+            {
+                ret = ret + "<hr/><table class='table table-hover'><thead><tr><th>DataSetName</th><th>V1</th><th>V2</th><th>V3</th></tr></thead><tbody>";
+            }
+
+            foreach (var line in dbret)
+            {
+                try
+                {
+                    var dsnm = Convert.ToString(line[0]);
+                    var v1 = Convert.ToString(line[1]);
+                    var v2 = Convert.ToString(line[2]);
+                    var v3 = Convert.ToString(line[3]);
+
+                    if (v2.ToUpper().Contains("PASS"))
+                    {
+                        ret = ret + "<tr style='color:green'><td>" + dsnm + "</td><td>"+v1+"</td><td>"+v2+"</td><td>"+v3+"</td></tr>";
+                    }
+                    else if (v2.ToUpper().Contains("FAIL"))
+                    {
+                        ret = ret + "<tr style='color:red'><td>" + dsnm + "</td><td>" + v1 + "</td><td>" + v2 + "</td><td>" + v3 + "</td></tr>";
+                    }
+                    else
+                    {
+                        ret = ret + "<tr><td>" + dsnm + "</td><td>" + v1 + "</td><td>" + v2 + "</td><td>" + v3 + "</td></tr>";
+                    }
+
+                }
+                catch (Exception ex) { }
+            }
+
+            if (dbret.Count > 0)
+            {
+                ret = ret + "</tbody></table>";
+            }
+
+            return ret;
+        }
+
         private static void CreateSystemIssues(List<ProjectTestData> failurelist)
         {
             if (failurelist.Count > 0)
@@ -404,7 +485,7 @@ namespace Prometheus.Models
                 {
                     var vm = new IssueViewModels();
                     vm.ProjectKey = item.ProjectKey;
-                    vm.IssueKey = item.DataID;
+                    vm.IssueKey = GetUniqKey(); //item.DataID;
                     vm.IssueType = ISSUETP.Bug;
                     vm.Summary = "Module " + item.ModuleSerialNum + " failed for " + item.ErrAbbr + " @ " + item.WhichTest;
                     vm.Priority = ISSUEPR.Major;
@@ -414,7 +495,7 @@ namespace Prometheus.Models
                     vm.Reporter = "System";
                     vm.Resolution = Resolute.Pending;
                     vm.ResolvedDate = DateTime.Parse("1982-05-06 01:01:01");
-                    vm.Description = "Module " + item.ModuleSerialNum + " failed for " + item.ErrAbbr + " @ " + item.WhichTest + " on tester " + item.TestStation + " " + item.TestTimeStamp.ToString("yyyy-MM-dd hh:mm:ss");
+                    vm.Description = "Module " + item.ModuleSerialNum + " failed for " + item.ErrAbbr + " @ " + item.WhichTest + " on tester " + item.TestStation + " " + item.TestTimeStamp.ToString("yyyy-MM-dd hh:mm:ss")+ DataFieldStr(item.DataID);
                     vm.CommentType = COMMENTTYPE.Description;
                     vm.ModuleSN = item.ModuleSerialNum;
                     vm.ErrAbbr = item.ErrAbbr;

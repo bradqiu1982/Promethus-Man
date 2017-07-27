0dc4de6c9485b1c2af213065e8f6b3214163fe3c
Brad Qiu <brad.qiu@finisar.com>
2017-07-27 13:48:11 +0800

    Try to add Http handle ,but failed

---
+++ Prometheus/Prometheus/App_Start/MYStaticFileHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prometheus
+{
+    public class MYStaticFileHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.Request.Path.ToUpper().Contains("/USERFILES/DOCS"))
+            {
+                var fooCookie = context.Request.Cookies["activenpi"];
+                if (fooCookie != null)
+                {
+
+                }
+            }
+        }
+    }
+}

6a9275499d01b07567334120da7150dd5e3b0807
yan.shi <yan.shi@WUX-L80008413>
2018-01-10 15:12:40 +0800

    try to change ckeditor 4.8 standard to 4.8 full

--- Prometheus/Prometheus/ckeditor/config.js
+++ Prometheus/Prometheus/ckeditor/config.js
@@ -12,20 +12,19 @@ CKEDITOR.editorConfig = function( config ) {
 	config.toolbarGroups = [
 		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
+		//{ name: 'links' },
         { name: 'insert'},
-		{ name: 'forms' },
+		//{ name: 'forms' },
 		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
+		{ name: 'document',	   groups: [ 'mode' ] },
+		//{ name: 'others' },
+		//'/',
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+        { name: 'styles' },
 		{ name: 'colors' },
 		{ name: 'about' }
-	];
-
+    ];
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
@@ -34,7 +33,7 @@ CKEDITOR.editorConfig = function( config ) {
 	config.format_tags = 'p;h1;h2;h3;pre';
 
 	// Simplify the dialog windows.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
+    config.removeDialogTabs = 'image:advanced;link:advanced';
 
 	config.pasteFromWordRemoveStyles = false;
     config.pasteFromWordRemoveFontStyles = false;

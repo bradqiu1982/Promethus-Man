a92ecd8e65c68076a49abad31bd18578bd230d56
Brad Qiu <brad.qiu@finisar.com>
2018-01-18 15:04:05 +0800

    remove links from ckediter will make the file link not working

--- Prometheus/Prometheus/ckeditor/config.js
+++ Prometheus/Prometheus/ckeditor/config.js
@@ -12,7 +12,7 @@ CKEDITOR.editorConfig = function( config ) {
 	config.toolbarGroups = [
 		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		//{ name: 'links' },
+		{ name: 'links' },
         { name: 'insert'},
 		//{ name: 'forms' },
 		{ name: 'tools' },
@@ -27,7 +27,7 @@ CKEDITOR.editorConfig = function( config ) {
     ];
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	config.removeButtons = 'Underline,Subscript,Superscript,Image';
 
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
